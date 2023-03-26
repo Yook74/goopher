4b419ae413f690cce68b15c2410fcc3a818b1b50
Andrew Blomenberg <andrewBlomen@gmail.com>
2023-03-26 09:57:07 -0600

    fleshed out audience look and base class

--- validators/audence_look.py
+++ validators/audence_look.py
@@ -1,5 +1,30 @@
+from typing import List
+import sys
+
 from .base_class import Validator
+from goof import Goof, LOG, MAJOR, MINOR
+sys.path.append('protobuf')
+from presentation_pb2 import Presentation
 
 
 class AudienceLookValidator(Validator):
-    pass
+    def validate_playlist(self, playlist: List[Presentation]) -> List[Goof]:
+        self.goofs = []
+
+        for pres in playlist:
+            if not len(pres.cues):
+                self.goofs.append(Goof(f'{pres.name} appears to be empty', LOG))
+                continue
+
+            look = None
+            for action in pres.cues[0].actions:
+                if action.name == 'Audience Look':
+                    look = action.audience_look
+
+            if look is None:
+                self.goofs.append(Goof(f'no audience look on the first slide of {pres.name}', MAJOR))
+
+            elif 'nothing' in look.identification.parameter_name.lower():
+                self.goofs.append(Goof(f'{pres.name} starts with stream nothing look', MINOR))
+
+        return self.goofs
--- validators/base_class.py
+++ validators/base_class.py
@@ -1,10 +1,16 @@
-from abc import ABC
-from typing import List
+from abc import ABC, abstractmethod
+from typing import List, Optional
+import sys
 
 sys.path.append('protobuf')
 from presentation_pb2 import Presentation
+from goof import Goof
 
 
 class Validator(ABC):
-    def validate_playlist(self, playlist: List[Presentation]):
+    def __init__(self):
+        self.goofs: Optional[List[Goof]] = None
+
+    @abstractmethod
+    def validate_playlist(self, playlist: List[Presentation]) -> List[Goof]:
         pass
