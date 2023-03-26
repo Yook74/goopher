0e49974bec608849777855ee6857ed6146c87a0b
Andrew Blomenberg <andrewBlomen@gmail.com>
2023-03-26 10:07:00 -0600

    Added gopher

--- check.py
+++ check.py
@@ -2,10 +2,25 @@ from argparse import ArgumentParser
 from parse import parse_proplaylist
 from validators import AudienceLookValidator, StickyPropValidator
 
+gopher = """         ,_---~~~~~----._         
+  _,,_,*^____      _____``*g*\"*, 
+ / __/ /'     ^.  /      \ ^@q   f 
+[  @f |  @))   |  |  @))  l  0 _/  
+ \`/   \~____ / __ \_____/    \   
+  |           _l__l_           I   
+  }          [______]          I  
+  ]            | | |           |  
+   ]            ~ ~           |  
+"""
+
 if __name__ == '__main__':
     parser = ArgumentParser(prog='goopher', description='Inspects ProPresenter playlists for errors (goofs)')
     parser.add_argument('filename', help='something ending in .proPlaylist')
     args = parser.parse_args()
+
+    print(gopher)
+    print(f'Checking {args.filename} for goofs')
+    print()
 
     playlist = parse_proplaylist(args.filename)
 
